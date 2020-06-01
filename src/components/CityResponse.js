7fadbbefbaa2a4b4d58a7b065292e1cdd3697324
Callum Herbert <cm.herbert@yahoo.co.nz>
2020-06-01 12:42:25 +0000

    Added Test Cases, Units
    * Commented out ZIP test cases
    + Added Relevant test cases for City Name input possibilities
    + Added metric units to displayed weather information

--- src/components/CityResponse.js
+++ src/components/CityResponse.js
@@ -22,27 +22,27 @@ function CityResponse(props) {
                     <tbody>
                         <tr>
                             <td>City</td>
-                            <td>{props.responseData.name}</td>
+                            <td>{props.responseData.name}, {props.responseData.sys.country}</td>
                         </tr>
                         <tr>
                             <td>Temperature</td>
-                            <td>{props.responseData.main.temp}</td>
+                            <td>{props.responseData.main.temp} °C</td>
                         </tr>
                         <tr>
                             <td>Pressure</td>
-                            <td>{props.responseData.main.pressure}</td>
+                            <td>{props.responseData.main.pressure} hPa</td>
                         </tr>
                         <tr>
                             <td>Humidity</td>
-                            <td>{props.responseData.main.humidity}</td>
+                            <td>{props.responseData.main.humidity} %</td>
                         </tr>
                         <tr>
                             <td>Temperature (Min)</td>
-                            <td>{props.responseData.main.temp_min}</td>
+                            <td>{props.responseData.main.temp_min} °C</td>
                         </tr>
                         <tr>
                             <td>Temperature (Max)</td>
-                            <td>{props.responseData.main.temp_max}</td>
+                            <td>{props.responseData.main.temp_max} °C</td>
                         </tr>
                         <tr>
                             <td>Conditions</td>
--- tests/fvt/nightwatch-tests/initial.js
+++ tests/fvt/nightwatch-tests/initial.js
@@ -10,7 +10,7 @@ module.exports = {
         .end(done);
     },
 
-    'Navigate to the DemoDOI - valid zip': async (browser) => {
+   /* 'Navigate to the DemoDOI - valid zip': async (browser) => {
         const demodoi = browser.page.demodoi();
         const { cityName } = demodoi.section;
     
@@ -55,4 +55,91 @@ module.exports = {
 
         demodoi.expect.element('@invalidCity').text.to.equal('* should be a 5 digit number only');
     },
+    */
+   'Navigate to the DemoDOI - valid city': async (browser) => {
+        const demodoi = browser.page.demodoi();
+        const { cityName } = demodoi.section;
+    
+        await demodoi.navigate().waitForElementVisible('@inputText');
+
+        await demodoi.setValue('@inputText', [
+            'Hamilton',
+            browser.Keys.ENTER
+          ]);
+    
+        await demodoi.waitForElementVisible('@table');
+
+        cityName.expect.element('@firstApp').text.to.equal('Hamilton, NZ');
+    },
+    'Navigate to the DemoDOI - Spaced valid input': async (browser) => {
+        const demodoi = browser.page.demodoi();
+        const { cityName } = demodoi.section;
+    
+        await demodoi.navigate().waitForElementVisible('@inputText');
+
+        await demodoi.setValue('@inputText', [
+            'Te Uku',
+            browser.Keys.ENTER
+          ]);
+    
+        await demodoi.waitForElementVisible('@table');
+
+        cityName.expect.element('@firstApp').text.to.equal('Te Uku, NZ');
+    },
+      'Navigate to the DemoDOI - invalid city': async (browser) => {
+        const demodoi = browser.page.demodoi();
+    
+        await demodoi.navigate().waitForElementVisible('@inputText');
+
+        await demodoi.setValue('@inputText', [
+            'Lake',
+            browser.Keys.ENTER
+          ]);
+    
+        await demodoi.waitForElementVisible('@table');
+
+         demodoi.expect.element('@cityNotFound').text.to.equal('city not found');
+    },
+     'Navigate to the DemoDOI - Numeric input': async (browser) => {
+        const demodoi = browser.page.demodoi();
+    
+        await demodoi.navigate().waitForElementVisible('@inputText');
+
+        await demodoi.setValue('@inputText', [
+            '12345',
+            browser.Keys.ENTER
+          ]);
+    
+        await demodoi.waitForElementVisible('@table');
+
+        demodoi.expect.element('@invalidCity').text.to.equal('* Input should only consit of letters, whitespaces and hypens');
+    },
+     'Navigate to the DemoDOI - Symbol input': async (browser) => {
+        const demodoi = browser.page.demodoi();
+    
+        await demodoi.navigate().waitForElementVisible('@inputText');
+
+        await demodoi.setValue('@inputText', [
+            'Hamilton?!',
+            browser.Keys.ENTER
+          ]);
+    
+        await demodoi.waitForElementVisible('@table');
+
+        demodoi.expect.element('@invalidCity').text.to.equal('* Input should only consit of letters, whitespaces and hypens');
+    },
+     'Navigate to the DemoDOI - Mixed Invalid input': async (browser) => {
+        const demodoi = browser.page.demodoi();
+    
+        await demodoi.navigate().waitForElementVisible('@inputText');
+
+        await demodoi.setValue('@inputText', [
+            'Hamilto-n?12',
+            browser.Keys.ENTER
+          ]);
+    
+        await demodoi.waitForElementVisible('@table');
+
+        demodoi.expect.element('@invalidCity').text.to.equal('* Input should only consit of letters, whitespaces and hypens');
+    }
 };
