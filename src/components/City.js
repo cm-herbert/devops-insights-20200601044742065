b69bebfe12e9cdeb589186193c07ac40df847ed0
Callum Herbert <cm.herbert@yahoo.co.nz>
2020-06-01 10:45:51 +0000

    Replaced Support for US Zip code based weather with support for New Zealand city based weather, System now displays information in Metric units App Header renamed to reflect above changes

---
+++ src/components/City.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function City(props) {
+
+    const [validationError, setValidationError] = useState(null);
+
+    const validate = (event) => {
+        const inputPattern = /^[(a-z)|(A-Z)|\s|-]*$/; //Match only letters and Spaces or hyphen (There is at least one city name with a hypen in NZ according to wikipedia)
+        const valid = inputPattern.test(event.target.value);
+        if (!valid) {
+            setValidationError('* Input should only consit of letters, whitespaces and hypens');
+            props.clearResponse();
+        } else {
+            setValidationError('');
+            props.onCityChange(event.target.value);
+        }
+    };
+
+    return (
+        <div className="col-sm-4">
+            <div className="row">
+                <div className="col-sm-10">
+                    <style jsx="true">{`
+                        .form-control::-webkit-input-placeholder {
+                            color: #ddd;
+                        }
+                    `}
+                    </style>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="usr" 
+                        placeholder="City / Town Name"
+                        onKeyPress={(event) => {
+                            if (event.key === "Enter") {
+                                validate(event);
+                            }
+                        }}
+                    ></input>   
+                </div>
+            </div>
+            <div className="pl-3 row">
+                <div className="text-danger small"> { validationError }</div>
+            </div>
+        </div>
+    );
+}
+
+export default City
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import fetch from 'isomorphic-unfetch'
-import ZipResponse from '../components/ZipResponse';
-import Zip from '../components/Zip';
+import CityResponse from '../components/CityResponse';
+import City from '../components/City';
 
 function AppContainer(props) {
 
-    const [responseData, setResponseData] = useState('');
+    const [responseData, setResponseData] = useState(''); 
 
-    const handleZipChange = async (zipValue) => {
+   
+   /*const handleZipChange = async (zipValue) => {
         //console.log(`--------- fetchData called zip:${zipValue}`)
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial&zip=${zipValue},us`)
+        const json = await res.json()
+        //console.log(json);
+        setResponseData(json);
+    }
+    */
+    
+    const handleTownChange = async (cityNZValue) => {
+        //console.log(`--------- fetchData called zip:${zipValue}`)
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&q=${cityNZValue},nz`)
         const json = await res.json()
         //console.log(json);
         setResponseData(json);
@@ -23,12 +33,12 @@ function AppContainer(props) {
         <div>
             <div className="row mt-4">
                 <div className="col-sm-4"></div>
-                <Zip onZipChange={handleZipChange} clearResponse={clearResponse}/>
+                <City onCityChange={handleTownChange} clearResponse={clearResponse}/>
                 <div className="col-sm-4"></div>
             </div>
             <div className="row mt-4">
                 <div className="col-sm-2"></div>
-                <ZipResponse responseData={responseData} clearResponse={clearResponse}/>
+                <CityResponse responseData={responseData} clearResponse={clearResponse}/>
                 <div className="col-sm-2"></div>
             </div>    
         </div>
